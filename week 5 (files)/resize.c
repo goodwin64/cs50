c06a38817b2235091cbe7a6bb37afeb58716362c
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-03-27 17:50:01 +0300

    Added tasks from week 5 (resize, full task)

--- week 5 (files)/resize.c
+++ week 5 (files)/resize.c
@@ -37,7 +37,80 @@ int main(int argc, char* argv[]) {
         return 4;
     }
 
-    // todo: add [parsing] files
+    // read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42
+        || bf.bfOffBits != 54
+        || bi.biSize != 40
+        || bi.biBitCount != 24
+        || bi.biCompression != 0) {
+            fclose(outptr);
+            fclose(inptr);
+            fprintf(stderr, "Unsupported file format.\n");
+            return 4;
+    }
+
+    // construct bitmap headers for the outfile
+    BITMAPFILEHEADER out_bf;
+    BITMAPINFOHEADER out_bi;
+    out_bf = bf;
+    out_bi = bi;
+    out_bi.biWidth = bi.biWidth * factor;
+    out_bi.biHeight = bi.biHeight * factor;
+
+    // determine padding for scanlines
+    // Needed for SizeImage calculation
+    int in_padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int out_padding =  (4 - (out_bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Calculate file and image size
+    // biSizeImage calculation.
+    out_bf.bfSize = 54 + out_bi.biWidth * abs(out_bi.biHeight) * 3 + abs(out_bi.biHeight) *  out_padding;
+    out_bi.biSizeImage = ((((out_bi.biWidth * out_bi.biBitCount) + 31) & ~31) / 8) * abs(out_bi.biHeight);
+
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&out_bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&out_bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++) {
+        // Write each line factor-times
+        for(int n = 0; n < factor; n++) {
+            // iterate over pixels in scanline
+            for (int j = 0; j < bi.biWidth; j++) {
+                // temporary storage
+                RGBTRIPLE triple;
+
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+                // write RGB triple to outfile
+                for(int m = 0; m < factor; m++) {
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                }
+            }
+
+            // skip over padding in infile
+            fseek(inptr, in_padding, SEEK_CUR);
+
+            // then add it to outfile
+            for (int k = 0; k <out_padding; k++) {
+                fputc(0x00, outptr);
+            }
+
+            fseek(inptr, -(bi.biWidth * 3 + in_padding ), SEEK_CUR);
+
+        }
+        fseek(inptr, bi.biWidth*3+in_padding, SEEK_CUR);
+    }
 
     // close infile
     fclose(inptr);
