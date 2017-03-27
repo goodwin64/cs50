e919a4c427b5192ff78590d1aadb97b1b83033b1
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-03-27 16:49:49 +0300

    Added tasks from week 5 (resize, open files)

--- week 5 (files)/resize.c
+++ week 5 (files)/resize.c
@@ -5,7 +5,45 @@
 
 int main(int argc, char* argv[]) {
 
-    // todo: impl
+    // ensure proper usage
+    if (argc != 4) {
+        printf("Usage:resize n infile outfile\n");
+        return 1;
+    }
+
+    // Check the resize factor
+    int factor = atoi(argv[1]); // parse int from console args
+    if (factor < 0 || factor > 100) {
+        printf("The resize factor should be a positive integer 0..100\n");
+        return 2;
+    }
+
+    // remember filenames and factor
+    char* infile = argv[2];
+    char* outfile = argv[3];
+
+    // open input file
+    FILE* inptr = fopen(infile, "r");
+    if (inptr == NULL) {
+        printf("Could not open file %s.\n", infile);
+        return 3;
+    }
+
+    // open output file
+    FILE* outptr = fopen(outfile, "w");
+    if (outptr == NULL) {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 4;
+    }
+
+    // todo: add [parsing] files
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
 
     return 0;
 }
